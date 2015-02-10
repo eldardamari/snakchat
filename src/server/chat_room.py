05dd8ba8f649c449e1c5c86cb90cf70948ac3a57
Eldar Damari <eldar.damari@gmail.com>
2015-02-10 20:01:37 +0200

    ADD - send personal messages, get client, and coloring

--- src/server/chat_room.py
+++ src/server/chat_room.py
@@ -18,10 +18,28 @@ class ChatRoom (object):
                     return True
             return False
 
+    def send_private_msg(self,username,msg):
+        self.lock.acquire()
+        self.get_client(username).send_msg(username,msg+"\n")
+        self.lock.release()
+
     def send_all(self,username,msg):
         self.lock.acquire()
 
         for client_it in self.clients:         
-            client_it.send_msg(username,msg)
+            client_it.send_msg(username,self.color_msg(username,msg))
 
         self.lock.release()
+
+    def color_msg(self,username,msg):
+        client = self.get_client(username)
+        if client != None:
+            return client.put_color(msg)
+        else:
+            return msg
+
+    def get_client(self,username):
+        for client_it in self.clients:
+            if client_it.get_username() == username:
+                return client_it
+        return None
