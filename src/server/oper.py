ead9dffd9981bf9263147e06e9876caaa37cbd3e
Eldar Damari <eldar.damari@gmail.com>
2015-02-10 09:47:11 +0200

    NEW - Oper will keep all clients and char room details

---
+++ src/server/oper.py
@@ -0,0 +1,22 @@
+from chat_room import ChatRoom
+
+class Oper (object):
+
+    def __init__(self):
+        print 'in oper'
+        self.clients = []
+        self.channels = ChatRoom("Master")
+
+
+    def add_client(self, client):
+        if client:
+            self.clients.append(client)
+            return True
+        else:
+            return False
+
+    def num_of_clients(self):
+        return len(self.clients)
+
+    def get_clients(self):
+        return self.clients 
