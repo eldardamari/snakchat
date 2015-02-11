4db3abacdca05476d0d114ae8415401d969bf9c4
Eldar Damari <eldar.damari@gmail.com>
2015-02-11 21:38:31 +0200

    ADD - Utils as global, NEW - EXIT command

--- src/server/protocol.py
+++ src/server/protocol.py
@@ -1,11 +1,6 @@
-colors = { "pink"  : '\033[95m',
-    "white"  : '\033[97m',
-    "blue"  : '\033[94m',
-    "green" : '\033[92m',
-    "yellow": '\033[93m',
-    "red"   : '\033[91m',
-    "sky"   : '\033[96m'
-    }
+import sys
+sys.path.append("..")
+import utils.utilities as Utils
 
 class Protocol (object):
     
@@ -25,47 +20,48 @@ class Protocol (object):
         return msg.split(" ",1)
 
     def is_command(self,cmd):
-        commands = ["HELP","MEMBERS","COLOR","BEEP","PRIVATE"]
+        commands = ["HELP","MEMBERS","COLOR","BEEP","PRIVATE","EXIT"]
         return cmd in commands 
 
     def get_command_results(self,splited,client):
         cmd = splited[0]
-        global colors
 
         if cmd == "HELP":
-            return self.color_system_msg(""" ** Menu - Options **
+            return Utils.color_system_msg(""" ** Menu - Options **
             - COLOR <color> - Changing text color, colors: {colors}
             - MEMBERS       - Prints all active users.
             - BEEP <status> - Activate beep sound for incoming messages, status: ON / OFF
-            - PRIVATE <username> <msg> - Send private messages to other users.""".format(colors=colors.keys()))
+            - PRIVATE <username> <msg> - Send private messages to other users.""".format(colors=Utils.colors.keys()))
 
         elif cmd == "MEMBERS":
            return "Active Users: "+"/".join(self.oper.get_usernames())
 
         elif cmd == "COLOR":
             if len(splited) == 2:
-                if splited[1] in colors:
-                    self.oper.set_client_color(client,colors[splited[1]])
-                    return self.color_system_msg("** Color changed succssefully to {color} **".format(color=splited[1]))
-            return self.color_system_msg("** Error changing your color, please choose between {colors} **".format(colors=colors.keys()))
+                if splited[1] in Utils.colors:
+                    self.oper.set_client_color(client,Utils.colors[splited[1]])
+                    return Utils.color_system_msg("** Color changed succssefully to {color} **".format(color=splited[1]))
+            return Utils.color_system_msg("** Error changing your color, please choose between {colors} **".format(colors=Utils.colors.keys()))
 
         elif cmd == "BEEP":
             if len(splited) == 2:
                 if splited[1] in ["ON","OFF"]:
                     self.oper.set_client_beep(client,splited[1])
-                    return self.color_system_msg("** Changed succssefully to {status} **".format(status=splited[1]))
-            return self.color_system_msg("** Error changing your color, please choose between {options} **".format(options=["ON","OFF"]))
+                    return Utils.color_system_msg("** Changed succssefully to {status} **".format(status=splited[1]))
+            return Utils.color_system_msg("** Error changing your color, please choose between {options} **".format(options=["ON","OFF"]))
 
         elif cmd == "PRIVATE":
             args = self.split_msg(splited[1])
             if len(args) >= 2:
                 client_to_send = self.oper.get_room().get_client(args[0])
                 if client_to_send != None:
-                    client_to_send.send_msg(client.put_color("<"+client.get_username()+"> ") + self.color_system_msg("<*Private*> ") + client.put_color("".join(args[1:])))
+                    client_to_send.send_msg(client.put_color("<"+client.get_username()+"> ") + Utils.color_system_msg("<*Private*> ") + client.put_color("".join(args[1:])))
                 else:
-                    return self.color_system_msg("** Error sending private message to <{username}>, user is not exist".format(username=splited[1]))
+                    return Utils.color_system_msg("** Error sending private message to <{username}>, user is not exist".format(username=args[0]))
             else:
-                return self.color_system_msg("** Error sending private message to <{username}>, user is not exist")
+                return Utils.color_system_msg("** Error sending private message to <{username}>, user is not exist")
 
-    def color_system_msg(self,msg):
-        return colors["sky"] + msg + "\033[0m"
+        elif cmd == "EXIT":
+            if len(splited) == 1:
+                self.oper.kill_client(client)
+            return ""
